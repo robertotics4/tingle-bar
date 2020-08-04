66ace7d464a7f9da5c5f8bc470f11bba8e76710d
robertotics4 <robertooliveiragp@gmail.com>
2020-08-04 01:10:48 -0300

    cadastro de funcionario, problema ao listar

--- src/pages/PainelEstabelecimento/pages/GerenciarFuncionarios/ListaFuncionarios.jsx
+++ src/pages/PainelEstabelecimento/pages/GerenciarFuncionarios/ListaFuncionarios.jsx
@@ -1,22 +1,134 @@
 import React, { useState, useEffect } from 'react';
+import Swal from 'sweetalert2';
 
 import api from '../../../../services/api';
 
 export default function ListaFuncionarios() {
     const [funcionarios, setFuncionarios] = useState([]);
+    const [tiposFuncionario, setTiposFuncionario] = useState([]);
+    const [estabelecimento, setEstabelecimento] = useState(null);
 
     useEffect(() => {
+        async function loadStoragedData() {
+            const storagedEstabelecimento = localStorage.getItem('@TBAuth:estabelecimento');
+
+            if (storagedEstabelecimento) {
+                setEstabelecimento(JSON.parse(storagedEstabelecimento));
+            }
+        }
+
+        loadStoragedData();
         getFuncionarios();
+        getTiposFuncionario();
     }, []);
 
     async function getFuncionarios() {
         try {
-            const response = await api.get('/Funcionario?idEstabelecimento=13');
+            const response = await api.get('/Funcionario?idEstabelecimento=' + estabelecimento.id_Estabelecimento);
             setFuncionarios(response.data);
             return response.data;
         } catch (err) {
             return err.response;
         }
+    }
+
+    async function getTiposFuncionario() {
+        try {
+            const response = await api.get('/tipofuncionario');
+            setTiposFuncionario(response.data);
+            return response.data;
+        } catch (err) {
+            return err.response;
+        }
+    }
+
+    async function getUsuario(cpf) {
+        try {
+            const response = await api.get('/usuario/GetByCpf?Cpf=' + cpf);
+            return response.data;
+        } catch (err) {
+            return err.response;
+        }
+    }
+
+    async function cadastrarFuncionario(idUsuario, idTipoFuncionario) {
+        const payload = {
+            "id_usuario": idUsuario,
+            "id_estabelecimento": estabelecimento.id_Estabelecimento,
+            "id_tipofuncionario": idTipoFuncionario
+        }
+
+        // try {
+        //     const response = await api.post('/Funcionario', payload);
+
+        //     if (response.status === 201 || response.status === 200) {
+        //         Swal.fire('Sucesso!', 'Funcionário cadastrado com sucesso!', 'success')
+        //     }
+
+        // } catch (err) {
+        //     console.log({ err });
+        //     if (err.response.status === 401 || err.response.status === 400) {
+        //         Swal.fire('Erro!', 'Falha ao cadastrar o funcionário', 'error')
+        //     }
+        // }
+    }
+
+    async function handleCadastrar() {
+        Swal.mixin({
+            input: 'text',
+            confirmButtonText: 'Pesquisar',
+            showCancelButton: true,
+            progressSteps: ['1'],
+            inputValidator: value => {
+                return new Promise(resolve => {
+                    if (value) {
+                        resolve()
+                    } else {
+                        resolve('CPF inválido');
+                    }
+                });
+            }
+        }).queue([
+            {
+                title: 'Pesquisar Usuário',
+                text: 'Digite o CPF'
+            }
+        ]).then(async result => {
+            if (result.value) {
+                try {
+                    const cpf = (result.value[0])
+                    const pesquisaUsuario = await getUsuario(cpf);
+
+                    if (pesquisaUsuario.cpf) {
+                        const { value } = Swal.fire({
+                            title: 'Cadastrar Funcionário',
+                            html: `Nome: ${pesquisaUsuario.nome}
+                                    <pre>cpf: ${pesquisaUsuario.cpf}</pre>`,
+                            input: 'select',
+                            inputOptions: tiposFuncionario.map(item => item.descricao),
+                            inputPlaceholder: 'Selecione a função',
+                            confirmButtonText: 'Cadastrar',
+                            showCancelButton: true,
+                            inputValidator: (value) => {
+                                return new Promise((resolve) => {
+                                    if (value)
+                                        resolve()
+                                    else
+                                        resolve('Você precisa selecionar uma função');
+                                });
+                            }
+                        }).then(result => {
+                            const idTipoFuncionario = tiposFuncionario[result.value].id;
+                            cadastrarFuncionario(pesquisaUsuario.id, idTipoFuncionario);
+                        });
+                    } else {
+                        Swal.fire('CPF não localizado', 'Tente novamente', 'error');
+                    }
+                } catch (err) {
+                    Swal.fire('CPF não localizado', 'Tente novamente', 'error');
+                }
+            }
+        });
     }
 
     return (
@@ -43,8 +155,6 @@ export default function ListaFuncionarios() {
             <section className="content">
                 <div className="container-fluid">
 
-
-
                     <div className="card">
                         <div className="card-header">
                             <h3 className="card-title">Lista de funcionários</h3>
@@ -61,13 +171,13 @@ export default function ListaFuncionarios() {
                                     <tr>
                                         <th style={{ width: '10%' }}>
                                             ID
-                                </th>
+                                        </th>
                                         <th style={{ width: '40%' }}>
                                             Nome
-                                </th>
+                                        </th>
                                         <th style={{ width: '20%' }}>
                                             Tipo
-                                </th>
+                                        </th>
                                         <th style={{ width: '30%' }}>
                                         </th>
                                     </tr>
@@ -88,22 +198,22 @@ export default function ListaFuncionarios() {
                                             </td>
 
                                             <td className="project-actions text-right">
-                                                <button className="btn btn-success btn-sm">
+                                                <button className="btn btn-success btn-sm" onClick={handleCadastrar}>
                                                     <i className="fas fa-user-plus mr-2"></i>
-                                            Novo
-                                        </button>
+                                                    Novo
+                                                </button>
                                                 <button className="btn btn-primary btn-sm ml-3">
                                                     <i className="fas fa-eye mr-2"></i>
-                                            Visualizar
-                                        </button>
+                                                    Visualizar
+                                                </button>
                                                 <button className="btn btn-info btn-sm ml-3">
                                                     <i className="fas fa-pencil-alt mr-2"></i>
-                                            Editar
-                                        </button>
+                                                    Editar
+                                                </button>
                                                 <button className="btn btn-danger btn-sm ml-3">
                                                     <i className="fas fa-trash mr-2"></i>
-                                            Deletar
-                                        </button>
+                                                    Deletar
+                                                </button>
                                             </td>
                                         </tr>
                                     })}
