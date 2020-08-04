46145d71f06121ee5c9fd3488d5352bb3f158aa8
robertotics4 <robertooliveiragp@gmail.com>
2020-08-04 17:47:34 -0300

    refatorando layout

--- src/pages/PainelEstabelecimento/pages/GerenciarMesas/ListaMesas.jsx
+++ src/pages/PainelEstabelecimento/pages/GerenciarMesas/ListaMesas.jsx
@@ -56,51 +56,46 @@ export default function ListaFuncionarios() {
                                 </thead>
                                 <tbody>
                                     <tr>
-                                        <td>Functional-requirements.docx</td>
-                                        <td>49.8005 kb</td>
+                                        <td>01</td>
+                                        <td>Disponível</td>
                                         <td className="text-right py-0 align-middle">
                                             <div className="btn-group btn-group-sm">
                                                 <a href="#" className="btn btn-info"><i className="fas fa-eye" /></a>
                                                 <a href="#" className="btn btn-danger"><i className="fas fa-trash" /></a>
                                             </div>
                                         </td>
-                                    </tr><tr>
-                                        <td>UAT.pdf</td>
-                                        <td>28.4883 kb</td>
+                                    </tr>
+                                    <tr>
+                                        <td>02</td>
+                                        <td>Ocupada</td>
                                         <td className="text-right py-0 align-middle">
                                             <div className="btn-group btn-group-sm">
                                                 <a href="#" className="btn btn-info"><i className="fas fa-eye" /></a>
                                                 <a href="#" className="btn btn-danger"><i className="fas fa-trash" /></a>
                                             </div>
                                         </td>
-                                    </tr><tr>
-                                        <td>Email-from-flatbal.mln</td>
-                                        <td>57.9003 kb</td>
+                                    </tr>
+                                    <tr>
+                                        <td>03</td>
+                                        <td>Disponível</td>
                                         <td className="text-right py-0 align-middle">
                                             <div className="btn-group btn-group-sm">
                                                 <a href="#" className="btn btn-info"><i className="fas fa-eye" /></a>
                                                 <a href="#" className="btn btn-danger"><i className="fas fa-trash" /></a>
                                             </div>
                                         </td>
-                                    </tr><tr>
-                                        <td>Logo.png</td>
-                                        <td>50.5190 kb</td>
+                                    </tr>
+                                    <tr>
+                                        <td>04</td>
+                                        <td>Ocupada</td>
                                         <td className="text-right py-0 align-middle">
                                             <div className="btn-group btn-group-sm">
                                                 <a href="#" className="btn btn-info"><i className="fas fa-eye" /></a>
                                                 <a href="#" className="btn btn-danger"><i className="fas fa-trash" /></a>
                                             </div>
                                         </td>
-                                    </tr><tr>
-                                        <td>Contract-10_12_2014.docx</td>
-                                        <td>44.9715 kb</td>
-                                        <td className="text-right py-0 align-middle">
-                                            <div className="btn-group btn-group-sm">
-                                                <a href="#" className="btn btn-info"><i className="fas fa-eye" /></a>
-                                                <a href="#" className="btn btn-danger"><i className="fas fa-trash" /></a>
-                                            </div>
-                                        </td>
-                                    </tr></tbody>
+                                    </tr>
+                                </tbody>
                             </table>
                         </div>
                         {/* /.card-body */}
