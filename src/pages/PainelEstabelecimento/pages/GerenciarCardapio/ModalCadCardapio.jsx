475d13dbece278f016a8ee4c374ee2fc103fc346
robertotics4 <robertooliveiragp@gmail.com>
2020-08-05 00:11:52 -0300

    formulário de cadastro item cardapio

--- src/pages/PainelEstabelecimento/pages/GerenciarCardapio/ModalCadCardapio.jsx
+++ src/pages/PainelEstabelecimento/pages/GerenciarCardapio/ModalCadCardapio.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from "react-hook-form";
 
 import './styles/ModalCadCardapio.css';
 
 export default function ModalCadCardapio() {
+    const [valores, setValores] = useState({});
+
+    const { register, handleSubmit, errors } = useForm();
+
+    const currencyFormatter = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' });
+
+    const onSubmit = data => {
+        setValores(data);
+        console.log(valores);
+    };
+
+    function handleChange(event) {
+        const { name, value } = event.target;
+        setValores({ ...valores, [name]: value });
+    }
+
     return (
         <div className="cad-cardapio-modal">
             <div className="card card-primary">
@@ -11,43 +28,129 @@ export default function ModalCadCardapio() {
                 </div>
                 {/* /.card-header */}
                 {/* form start */}
-                <form role="form">
+
+                <form autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
+
                     <div className="card-body">
                         <div className="form-group">
                             <label htmlFor="idItemCardapio">ID</label>
-                            <input type="text" className="form-control" id="idItemCardapio" placeholder="ID" />
+                            <input
+                                name="idItemCardapio"
+                                type="text"
+                                className={errors.idItemCardapio ? "form-control is-invalid" : "form-control"}
+                                id="idItemCardapio"
+                                placeholder="ID"
+                                onChange={handleChange}
+                                ref={register({
+                                    required: {
+                                        value: "Required",
+                                        message: "O ID é obrigatório"
+                                    }
+                                })}
+                            />
+                            <span className="error invalid-feedback">{errors.idItemCardapio && errors.idItemCardapio.message}</span>
                         </div>
                         <div className="form-group">
                             <label htmlFor="tituloItemCardapio">Título</label>
-                            <input type="text" className="form-control" id="tituloItemCardapio" placeholder="Título do item" />
+                            <input
+                                name="tituloItemCardapio"
+                                type="text"
+                                className={errors.tituloItemCardapio ? "form-control is-invalid" : "form-control"}
+                                id="tituloItemCardapio"
+                                placeholder="Título do item"
+                                ref={register({
+                                    required: {
+                                        value: "Required",
+                                        message: "O título é obrigatório"
+                                    },
+                                })}
+                            />
+                            <span className="error invalid-feedback">{errors.tituloItemCardapio && errors.tituloItemCardapio.message}</span>
                         </div>
                         <div className="form-group">
                             <label htmlFor="descricaoItemCardapio">Descrição</label>
-                            <input type="text" className="form-control" id="descricaoItemCardapio" placeholder="Descrição do item" />
+                            <input
+                                name="descricaoItemCardapio"
+                                type="text"
+                                className={errors.descricaoItemCardapio ? "form-control is-invalid" : "form-control"}
+                                id="descricaoItemCardapio"
+                                placeholder="Descrição do item"
+                                ref={register({
+                                    required: {
+                                        value: "Required",
+                                        message: "A descrição é obrigatória"
+                                    },
+                                })}
+                            />
+                            <span className="error invalid-feedback">{errors.descricaoItemCardapio && errors.descricaoItemCardapio.message}</span>
                         </div>
                         <div className="form-group">
                             <label htmlFor="valorItemCardapio">Valor</label>
-                            <input type="text" className="form-control" id="valorItemCardapio" placeholder="Valor do item" />
+                            <input
+                                name="valorItemCardapio"
+                                placeholder="R$ 00.00"
+                                type="text"
+                                className={errors.valorItemCardapio ? "form-control is-invalid" : "form-control"}
+                                id="valorItemCardapio"
+                                placeholder="Valor do item"
+                                mask="R$ 99999.99"
+                                ref={register({
+                                    required: {
+                                        value: "Required",
+                                        message: "O valor é obrigatório"
+                                    },
+                                })}
+                            />
+                            <span className="error invalid-feedback">{errors.valorItemCardapio && errors.valorItemCardapio.message}</span>
                         </div>
                         <div className="form-group">
                             <label htmlFor="categoriaItemCardapio">Categoria</label>
-                            <select className="form-control" id="categoriaItemCardapio">
+                            <select
+                                name="categoriaItemCardapio"
+                                className={errors.categoriaItemCardapio ? "form-control is-invalid" : "form-control"}
+                                id="categoriaItemCardapio"
+                                ref={register({
+                                    required: {
+                                        value: "Required",
+                                        message: "A categoria é obrigatória"
+                                    },
+                                })}
+                            >
+                                <option value="">Selecione</option>
                                 <option>Categoria 1</option>
                                 <option>Categoria 2</option>
                                 <option>Categoria 3</option>
                             </select>
+                            <span className="error invalid-feedback">{errors.categoriaItemCardapio && errors.categoriaItemCardapio.message}</span>
                         </div>
                         <div className="form-group">
                             <label htmlFor="imagemItemCardapio">Imagem do item</label>
                             <div className="input-group">
                                 <div className="custom-file">
-                                    <input type="file" className="custom-file-input" id="imagemItemCardapio" />
+                                    <input
+                                        name="imagemItemCardapio"
+                                        type="file"
+                                        className={errors.imagemItemCardapio ? "custom-file-input is-invalid" : "custom-file-input"}
+                                        id="imagemItemCardapio"
+                                        ref={register({
+                                            required: {
+                                                value: "Required",
+                                                message: "A imagem é obrigatória"
+                                            },
+                                        })}
+                                    />
                                     <label className="custom-file-label" htmlFor="exampleInputFile">Selecione a imagem</label>
                                 </div>
                             </div>
+                            <span className="error invalid-feedback">{errors.imagemItemCardapio && errors.imagemItemCardapio.message}</span>
                         </div>
                         <div className="form-check">
-                            <input type="checkbox" className="form-check-input" id="itemCozinhaCardapio" />
+                            <input
+                                name="itemCozinhaCardapio"
+                                type="checkbox"
+                                className="form-check-input"
+                                id="itemCozinhaCardapio"
+                            />
                             <label className="form-check-label" htmlFor="exampleCheck1">Item de cozinha ?</label>
                         </div>
                     </div>
