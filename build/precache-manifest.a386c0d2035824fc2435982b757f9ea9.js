607183f6fd491fa3fcf05a8bf8edf7d98707bb4f
robertotics4 <robertooliveiragp@gmail.com>
2020-08-10 22:54:30 -0300

    teste deploy

---
+++ build/precache-manifest.a386c0d2035824fc2435982b757f9ea9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df69cb3e8be382dfe00d2dc1caac8c28",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e19c4f99197e414fef14",
+    "url": "./static/css/2.d97600f8.chunk.css"
+  },
+  {
+    "revision": "244af01a740046810198",
+    "url": "./static/css/main.14cdc755.chunk.css"
+  },
+  {
+    "revision": "e19c4f99197e414fef14",
+    "url": "./static/js/2.9180440e.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "./static/js/2.9180440e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "244af01a740046810198",
+    "url": "./static/js/main.fab95f2d.chunk.js"
+  },
+  {
+    "revision": "11872759ad16b3707628",
+    "url": "./static/js/runtime-main.1fd8dcc6.js"
+  },
+  {
+    "revision": "eeabcdcb0ba9eaa54d84bb21be9a5cc4",
+    "url": "./static/media/logo.eeabcdcb.png"
+  }
+]);
