9104f7abff5a732e1660ab96116285554e4a47ee
robertotics4 <robertooliveiragp@gmail.com>
2020-08-13 16:33:31 -0300

    listagem de pedidos finalizada

--- src/pages/PainelEstabelecimento/pages/GerenciarCozinha/components/ModalVisualizarPedido.jsx
+++ src/pages/PainelEstabelecimento/pages/GerenciarCozinha/components/ModalVisualizarPedido.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Modal, Image } from 'react-bootstrap';
 
-import './styles/ModalVisualizar.css';
+import '../styles/ModalVisualizarPedido.css';
 
 const baseURL = 'http://52.45.128.89';
 
-export default function ModalVisualizar(props) {
-    const currencyFormatter = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' });
+export default function ModalVisualizarPedido(props) {
+    const dateFormater = new Intl.DateTimeFormat('pt-BR');
     const handleClose = () => props.setShowModal(false);
 
     return (
@@ -21,13 +21,12 @@ export default function ModalVisualizar(props) {
                 <Modal.Body>
                     <div className="card-body">
                         <div className="content-item">
-                            <Image className="img-item" src={baseURL + props.item.imagem} fluid />
+                            <Image className="img-item" src={baseURL + props.item.item_imagem} fluid />
                             <h4 className="mt-3"><strong>{props.item.titulo}</strong></h4>
-                            <p className="texto-item">{props.item.item}</p>
-                            <p className="texto-item">
-                                Preparo: {props.item.tempo_estimado_min}{props.item.tempo_estimado_max ? ` a ${props.item.tempo_estimado_max}` : ''} minutos
-                            </p>
-                            <p className="valor-item">{currencyFormatter.format(props.item.valor)}</p>
+                            <p className="texto-item">{props.item.descricao}</p>
+                            <p className="texto-item">Categoria: {props.item.categoria}</p>
+                            <p className="texto-item">Quantidade: {props.item.item_qtd}</p>
+                            <p className="texto-item">Prazo máximo para entrega: {props.item.item_prev_max}</p>
                         </div>
                     </div>
                 </Modal.Body>
--- src/pages/PainelEstabelecimento/pages/GerenciarCozinha/components/Pedido.jsx
+++ src/pages/PainelEstabelecimento/pages/GerenciarCozinha/components/Pedido.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import '../styles/Pedido.css';
 
+import ModalVisualizarPedido from './ModalVisualizarPedido';
+
 export default function Pedido(props) {
+    const [showModalVisualizar, setModalVisualizar] = useState(false);
+    const [itemSelecionado, setItemSelecionado] = useState(null);
+
+    function handleVisualizar(item) {
+        setItemSelecionado(item);
+        setModalVisualizar(true);
+    }
+
     return (
         <div className="row">
             <div className="col-sm-12">
@@ -16,40 +26,6 @@ export default function Pedido(props) {
                             <div className="text-header">HORÁRIO: <i>{props.pedido.pedido_datahora}</i></div>
                         </div>
                     </div>
-                    {/* /.card-header */}
-                    {/* <div className="card-body">
-                        <table className="table">
-                            <thead className="tabela-cabecalho">
-                                <tr>
-                                    <th scope="col">Qtd</th>
-                                    <th scope="col">Produto</th>
-                                    <th scope="col">Categoria</th>
-                                    <th scope="col">Previsão</th>
-                                    <th scope="col"></th>
-                                </tr>
-                            </thead>
-                            <tbody className="tabela-corpo">
-                                {props.pedido.itens.map((item, index) => {
-                                    return <tr key={index}>
-                                        <th scope="row">{item.item_qtd}</th>
-                                        <td>{item.titulo}</td>
-                                        <td>{item.categoria}</td>
-                                        <td>{item.item_prev_max}</td>
-                                        <td><button className="btn btn-light btn-sm btn-block"><i className="fas fa-eye"></i><span className="ml-2">Detalhes</span></button></td>
-                                    </tr>
-                                })}
-                            </tbody>
-                        </table>
-                        <div className="mt-3">
-                            <button className="btn btn-success btn-block">
-                                <i className="fas fa-check-circle"></i>
-                                <span className="ml-2">FINALIZAR PEDIDO</span>
-                            </button>
-                        </div>
-                    </div> */}
-                    {/* /.card-body */}
-
-
 
                     <div className="card-body table-responsive p-0">
                         <table className="table table-striped table-valign-middle">
@@ -64,24 +40,37 @@ export default function Pedido(props) {
                             </thead>
                             <tbody>
 
-                                {props.pedido.itens.map((item, index) => {                                    
+                                {props.pedido.itens.map((item, index) => {
                                     return <tr key={index}>
                                         <th scope="row">{item.item_qtd}</th>
                                         <td>{item.titulo}</td>
                                         <td>{item.categoria}</td>
                                         <td>{item.item_prev_max}</td>
-                                        <td><a className="text-muted"><i className="fas fa-eye" onClick={() => {alert('ahay')}}></i></a></td>
+                                        <td><a className="text-muted"><i className="fas fa-eye" onClick={() => handleVisualizar(item)}></i></a></td>
                                     </tr>
-                       
+
                                 })}
 
                             </tbody>
                         </table>
+
+                        <div className="d-flex flex-row justify-content-end">
+                            <button className="btn btn-success btn-finalizar col-sm-12 col-md-3 col-lg-3">
+                                <i className="fas fa-check-circle"></i><span className="ml-2">FINALIZAR PEDIDO</span>
+                            </button>
+                        </div>
                     </div>
                 </div>
-                {/* /.card */}
             </div>
 
+            {showModalVisualizar
+                ? <ModalVisualizarPedido
+                    showModal={showModalVisualizar}
+                    setShowModal={setModalVisualizar}
+                    item={itemSelecionado}
+                />
+                : null
+            }
         </div>
     );
 }
