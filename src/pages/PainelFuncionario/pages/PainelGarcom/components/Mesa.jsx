b32e68d6eb0cd9aab85f11dcb0d731b29fb81600
robertotics4 <robertooliveiragp@gmail.com>
2020-08-28 02:02:17 -0300

    Hover btn fechar conta

--- src/pages/PainelFuncionario/pages/PainelGarcom/components/Mesa.jsx
+++ src/pages/PainelFuncionario/pages/PainelGarcom/components/Mesa.jsx
@@ -7,21 +7,25 @@ export default function Mesa(props) {
 
     return (
         <div className="card card-mesa" style={{ width: '18rem' }}>
-            <div className="card-body">
+            <div className="card-body p-3">
                 <div className="cabecalho-mesa">
                     <h5 className="card-title mr-2"><strong>{props.conta.desc_mesa.toUpperCase()}</strong></h5>
-                    <h5><span className="badge badge-danger">4</span></h5>
+                    <h4><span className="badge badge-danger">4</span></h4>
                 </div>
 
             </div>
             <ul className="list-group list-group-flush">
                 <li className="list-group-item">
-                    <i className="fas fa-id-badge mr-2 icone-mesa"></i>
-                    <span className="card-text">Atendente: {props.conta.garcom}</span>
+                    <div className="txt-comprimido">
+                        <i className="fas fa-id-badge mr-2 icone-mesa"></i>
+                        <span className="card-text">Atendente: {props.conta.garcom}</span>
+                    </div>
                 </li>
                 <li className="list-group-item">
-                    <i className="fas fa-smile mr-2 icone-mesa"></i>
-                    <span className="card-text">Cliente: {props.conta.usuarios[0].nome_usuario}</span>
+                    <div className="txt-comprimido">
+                        <i className="fas fa-smile mr-2 icone-mesa"></i>
+                        <span className="card-text">Cliente: {props.conta.usuarios[0].nome_usuario}</span>
+                    </div>
                 </li>
                 <li className="list-group-item">
                     <i className="fas fa-file-invoice-dollar mr-2 icone-mesa"></i>
@@ -30,8 +34,9 @@ export default function Mesa(props) {
             </ul>
 
             <button type="button" className="btn-fechar" onClick={props.onClick}>
+                <i className="fas fa-dollar-sign mr-2"></i>
                 Fechar conta
-                </button>
+            </button>
 
         </div>
     );
