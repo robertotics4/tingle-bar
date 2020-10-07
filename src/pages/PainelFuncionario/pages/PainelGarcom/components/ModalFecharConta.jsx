2acc3376e00da02fc273415a3f7b52fc34c0b065
robertotics4 <robertooliveiragp@gmail.com>
2020-10-07 01:17:41 -0300

    pagamento parcial finalizado

--- src/pages/PainelFuncionario/pages/PainelGarcom/components/ModalFecharConta.jsx
+++ src/pages/PainelFuncionario/pages/PainelGarcom/components/ModalFecharConta.jsx
@@ -5,6 +5,8 @@ import api from '../../../../../services/api';
 
 import '../styles/ModalVisualizarMesa.css';
 
+import { currencyMask } from '../../../../../utils/masks';
+
 export default function ModalFecharConta(props) {
     const [meiosPagamento, setMeiosPagamento] = useState([]);
     const currencyFormatter = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' });
@@ -24,33 +26,7 @@ export default function ModalFecharConta(props) {
         }
     }
 
-    async function fecharConta(a) {
-        Swal.fire({
-            title: 'Deseja fechar a conta?',
-            text: "A conta será fechada",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#d33',
-            cancelButtonColor: '#3085d6',
-            confirmButtonText: 'Fechar'
-        }).then(async result => {
-            if (result.value) {
-                try {
-                    const response = await api.post("Conta/Fecharconta?idConta=" + a.num_conta + "&total=" + a.valor_total_conta);
-                    if (response.status === 201 || response.status === 200) {
-                        Swal.fire('Sucesso!', 'Conta Fechada com sucesso!', 'success');
-                        props.atualizarItens();
-                        handleClose(true);
-
-                    }
-                } catch (err) {
-                    Swal.fire('Erro!', 'Falha ao fechar conta', 'error');
-                }
-            }
-        });
-    }
-
-    async function fecharContaParcial(usuario) {
+    async function fecharConta(conta) {
         Swal.mixin({
             input: 'text',
             confirmButtonText: 'Próximo',
@@ -59,13 +35,10 @@ export default function ModalFecharConta(props) {
         }).queue([
             {
                 title: 'Valor',
-                text: 'Informe o valor pago',
-                inputValue: currencyFormatter.format(usuario.valor_conta_usuario),
-                inputPlaceholder: 'R$',
-                inputValidator: value => {
-                    if (!value) {
-                        return 'Favor digitar o valor!'
-                    }
+                text: 'Valor total da conta',
+                inputValue: currencyFormatter.format(conta.valor_total_pago),
+                inputAttributes: {
+                    disabled: true
                 },
             },
             {
@@ -89,15 +62,70 @@ export default function ModalFecharConta(props) {
                 const indiceMP = parseInt(result.value[1]);
                 let total = result.value[0].replace('R$', '');
                 total = parseFloat(total.replace(',', '.'));
-                
-                const payload = {
-                    "idUsuarioConta": usuario.id_usuario_conta,
-                    "total": total,
-                    "meioPagamento": meiosPagamento[indiceMP].id_Meio_Pagamento
+
+                // try {
+                //     const response = await api.post(`/Conta/Fecharconta?idConta=${conta.num_conta}&idUsuarioConta=36&total=${total}&meioPagamento=${meiosPagamento[indiceMP].id_Meio_Pagamento}`);
+
+                //     if (response.status === 201 || response.status === 200) {
+                //         Swal.fire('Sucesso!', 'Pagamento efetuado com sucesso!', 'success');
+                //     }
+                // } catch (err) {
+                //     if (err.response.status === 401 || err.response.status === 400) {
+                //         Swal.fire('Erro!', 'Falha no pagamento', 'error');
+                //     }
+                // }
+            }
+        });
+    }
+
+    async function fecharContaParcial(usuario) {
+        Swal.mixin({
+            input: 'text',
+            confirmButtonText: 'Próximo',
+            showCancelButton: true,
+            progressSteps: ['1', '2']
+        }).queue([
+            {
+                title: 'Valor',
+                text: 'Informe o valor pago',
+                inputValue: currencyFormatter.format(usuario.valor_conta_usuario).replace('R$', ''),
+                inputPlaceholder: 'R$ 00,00',
+                inputValidator: value => {
+                    if (!value) {
+                        return 'Favor digitar o valor!'
+                    }
+                },
+                onOpen: () => {
+                    const input = Swal.getInput();
+                    input.oninput = (event) => {
+                        event.target.value = currencyMask(event.target.value);
+                    }
                 }
+            },
+            {
+                title: 'Forma de pagamento',
+                text: 'Selecione a forma de pagamento',
+                input: 'select',
+                inputPlaceholder: 'Selecione',
+                confirmButtonText: 'Confirmar',
+                inputOptions: meiosPagamento.map(meioPagamento => meioPagamento.descricao),
+                inputValidator: (value) => {
+                    return new Promise((resolve) => {
+                        if (value)
+                            resolve()
+                        else
+                            resolve('Você precisa selecionar uma opção');
+                    });
+                }
+            },
+        ]).then(async result => {
+            if (result.value) {
+                const indiceMP = parseInt(result.value[1]);
+                let total = result.value[0].replace('R$', '');
+                total = parseFloat(total.replace(',', '.'));
 
                 try {
-                    const response = await api.post('/Conta/FecharcontaParcial', payload);
+                    const response = await api.post(`/Conta/FecharcontaParcial?idUsuarioConta=${usuario.id_usuario_conta}&total=${total}&meioPagamento=${meiosPagamento[indiceMP].id_Meio_Pagamento}`);
 
                     if (response.status === 201 || response.status === 200) {
                         Swal.fire('Sucesso!', 'Pagamento efetuado com sucesso!', 'success');
@@ -116,21 +144,21 @@ export default function ModalFecharConta(props) {
         let lista = [];
 
         usuarios.forEach(usuario => {
-            //if (usuarios.length > 1) {
-            lista.push(
-                <tr className="table-secondary" key={usuario.usuario_id}>
-                    <td colSpan="3">
-                        <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-                            <strong>{usuario.nome_usuario.toUpperCase()}</strong>
-                            <button type="button" className="btn btn-success btn-sm" onClick={() => fecharContaParcial(usuario)}>
-                                <strong>Receber</strong>
-                                <i className="ml-2 fas fa-hand-holding-usd"></i>
-                            </button>
-                        </div>
-                    </td>
-                </tr>
-            );
-            //}
+            // if (usuarios.length > 1) {
+                lista.push(
+                    <tr className="table-secondary" key={usuario.cpf_usuario}>
+                        <td colSpan="3">
+                            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+                                <strong>{usuario.nome_usuario.toUpperCase()}</strong>
+                                <button type="button" className="btn btn-success btn-sm" onClick={() => fecharContaParcial(usuario)}>
+                                    <strong>Receber</strong>
+                                    <i className="ml-2 fas fa-hand-holding-usd"></i>
+                                </button>
+                            </div>
+                        </td>
+                    </tr>
+                );
+            // }
             usuario.pedidos.forEach(pedido => {
                 pedido.itens.forEach(item => {
                     lista.push(
@@ -186,6 +214,7 @@ export default function ModalFecharConta(props) {
 
                 <Modal.Footer className="footer-content">
                     <div className="footer">
+                        <span className="texto-total">TOTAL PAGO: {currencyFormatter.format(props.conta.valor_total_pago)}</span>
                         <span className="texto-total">TOTAL A PAGAR: {currencyFormatter.format(props.conta.valor_total_conta)}</span>
                         <button className="btn btn-info btn-block" onClick={() => fecharConta(props.conta)}>
                             <i className="fas fa-file-invoice-dollar mr-2"  ></i>
